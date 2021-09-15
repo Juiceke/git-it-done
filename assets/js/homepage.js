b2b1817f04a0c4073edbd4c4b499e27aa1475b0a
Brett herrell <brett_herrell@yahoo.com>
2021-09-15 15:51:50 -0500

    added buttons to search via langauge

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -3,6 +3,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector('#username');
 var repoContainerEl = document.querySelector('#repos-container');
 var repoSearchTerm = document.querySelector('#repo-search-term');
+var languageButtonsEl = document.querySelector('#language-buttons')
 
 var displayRepos = function(repos, searchTerm) {
     if (repos.length === 0) {
@@ -80,4 +81,28 @@ var getUserRepos = function(user) {
 });
 }
 
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayRepos(data.items, language)
+            });
+        } else {
+            alert('Error: Github User Not Found')
+        }
+    });
+}
+
+var buttonClickHandler = function(event) {
+var language = event.target.getAttribute('data-language')
+if (language) {
+    getFeaturedRepos(language);
+
+    repoContainerEl.textContent = "";
+}
+}
+
 userFormEl.addEventListener('submit', formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
